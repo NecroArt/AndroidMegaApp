1e9dd93a4026d270ce79acaa6d7dbb1b669bf1b2
Artyom <necroart@myopera.com>
2013-09-12 17:35:05 +1100

    sms read example

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myfirstapp;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.content.Intent;
+import android.database.Cursor;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
@@ -29,8 +31,29 @@ public class MainActivity extends Activity {
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
+    	//String message = editText.getText().toString();
+    	String message = readTestSms();
     	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
     }
+    
+    public String readTestSms() {
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/draft"), null, null, null, null);
+    	cursor.moveToFirst();
+    	
+    	String msgData = "";
+    	
+    	do{
+    		for(int idx=0;idx<cursor.getColumnCount();idx++)
+    		{
+    			//msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+    			if (cursor.getColumnName(idx).compareTo("body") == 0) {
+    				msgData = cursor.getString(idx);
+    			}
+    		}
+    	}while(cursor.moveToNext());
+
+    	return msgData;
+    }
+    
 }
