9ff7668dcdb2e6d7534abcdc68b354648d88baf8
Artyom DELL <necroart@myopera.com>
2013-09-30 08:35:19 +1100

    showing first catch sms

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.View;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
@@ -42,7 +43,50 @@ public class MainActivity extends Activity {
     }
     
     public String readTestSms() {
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/draft"), null, null, null, null);
+    	
+    	//------------------
+    	StringBuilder smsBuilder = new StringBuilder();
+        final String SMS_URI_INBOX = "content://sms/inbox";
+        final String SMS_URI_ALL = "content://sms/";
+         try {  
+             Uri uri = Uri.parse(SMS_URI_INBOX);  
+             String[] projection = new String[] { "_id", "address", "person", "body", "date", "type" };  
+             Cursor cur = getContentResolver().query(uri, projection, null/*where_clause*/, null, "date desc");
+              if (cur.moveToFirst()) {  
+                 int index_Address = cur.getColumnIndex("address");  
+                 int index_Person = cur.getColumnIndex("person");  
+                 int index_Body = cur.getColumnIndex("body");  
+                 int index_Date = cur.getColumnIndex("date");  
+                 int index_Type = cur.getColumnIndex("type");         
+                 //do {  
+                     String strAddress = cur.getString(index_Address);  
+                     int intPerson = cur.getInt(index_Person);  
+                     String strbody = cur.getString(index_Body);  
+                     long longDate = cur.getLong(index_Date);  
+                     int int_Type = cur.getInt(index_Type);  
+
+                     smsBuilder.append("[ ");  
+                     smsBuilder.append(strAddress + ", ");  
+                     smsBuilder.append(intPerson + ", ");  
+                     smsBuilder.append(strbody + ", ");  
+                     smsBuilder.append(longDate + ", ");  
+                     smsBuilder.append(int_Type);  
+                     smsBuilder.append(" ]\n\n");  
+                 //} while (cur.moveToNext());  
+
+                 //if (!cur.isClosed()) {  
+                     cur.close();  
+                     cur = null;  
+                 //}  
+             } else {  
+                 smsBuilder.append("no result!");
+             } // end if
+         }
+         catch (SQLiteException ex) {  
+             System.out.println("SQLiteException" + ex.getMessage());
+         }  
+    	//------------------
+        /*Cursor cursor = getContentResolver().query(Uri.parse("content://sms/draft"), null, null, null, null);
     	cursor.moveToFirst();
     	
     	String msgData = "";
@@ -57,7 +101,8 @@ public class MainActivity extends Activity {
     		}
     	}while(cursor.moveToNext());
 
-    	return msgData;
+    	return msgData;*/
+	return smsBuilder.toString();
     }
     
     public List<String> getSMS(){
