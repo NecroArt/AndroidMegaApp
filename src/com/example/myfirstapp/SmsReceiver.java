405938a6f6cee48983d913c8b1175706b817377c
Artem <artvc@myopera.com>
2013-10-08 11:20:52 +1100

    show sms content on receive

--- src/com/example/myfirstapp/SmsReceiver.java
+++ src/com/example/myfirstapp/SmsReceiver.java
@@ -16,7 +16,7 @@ public class SmsReceiver  extends BroadcastReceiver{
 	        // TODO Auto-generated method stub
 
 	        //if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-	            /*Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+	            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
 	            SmsMessage[] msgs = null;
 	            String msg_from;
 	            if (bundle != null){
@@ -24,7 +24,7 @@ public class SmsReceiver  extends BroadcastReceiver{
 	                try{
 	                    Object[] pdus = (Object[]) bundle.get("pdus");
 	                    msgs = new SmsMessage[pdus.length];
-	                    String allMEssages = "no text";
+	                    String allMEssages = "";
 	                    for(int i=0; i<msgs.length; i++){
 	                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
 	                        msg_from = msgs[i].getOriginatingAddress();
@@ -32,13 +32,12 @@ public class SmsReceiver  extends BroadcastReceiver{
 	                        	                        
 	                        allMEssages += msgBody; 
 	                    }
-	                    Toast.makeText(context, allMEssages, Toast.LENGTH_SHORT).show();
+	                    Toast.makeText(context, (allMEssages.length() == 0 ? "no text" : allMEssages), Toast.LENGTH_SHORT).show();
 	                    
 	                }catch(Exception e){
 	                	//Log.d("Exception caught",e.getMessage());
 	                }
-	            }*/
+	            }
 	        //}
-	    	Toast.makeText(context, "onReceive invoked", Toast.LENGTH_SHORT).show();
 	    }
 }
