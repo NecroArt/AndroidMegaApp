8ec615eb4f91f14b8dd4cd09ca0ba46404483af0
Artem <artvc@myopera.com>
2013-11-15 15:43:23 +1100

    cosmetical changes

--- src/com/view/DisplayPanesActivity.java
+++ src/com/view/DisplayPanesActivity.java
@@ -10,171 +10,288 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 public class DisplayPanesActivity extends Activity {
 
-	final Integer daysAmount = 5;
+	private static int daysAmount = 5;
 
-	@SuppressLint("NewApi")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.panel);
 
-		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			// Show the Up button in the action bar.
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-
-		DbHelper dbHelper = new DbHelper(this, null, null,
-				DbHelper.getDBVersion());
-
-		ArrayList<Integer> proceedsStatus = new ArrayList<Integer>();
-		ArrayList<Integer> operationalStatus = new ArrayList<Integer>();
-		ArrayList<Integer> dayNumber = new ArrayList<Integer>();
-
-		ArrayList<SmsRecord> recordsArray = dbHelper.getSMSRecordArrayList(
-				this, daysAmount);
-
-		String lastSmsId = "";
-		for (SmsRecord currentRecord : recordsArray) {
-			if (currentRecord.getParameterName().equals("ВЫРУЧКА")) {
-				if (currentRecord.getParameterValue().equals("вчера")) {
-					proceedsStatus.add(0);
-				} else {
-					if (currentRecord.getParameterValue().equals("позавчера")) {
-						proceedsStatus.add(1);
-					} else {
-						proceedsStatus.add(2);
-					}
-				}
-				dayNumber.add(currentRecord.getDate().get(Calendar.DAY_OF_MONTH));
-			} else {
-				if (currentRecord.getParameterName().equals("ОПЕРАТИВНЫЙ")) {
-					if (currentRecord.getParameterValue().equals("вчера")) {
-						operationalStatus.add(0);
-					} else {
-						if (currentRecord.getParameterValue().equals(
-								"позавчера")) {
-							operationalStatus.add(1);
-						} else {
-							operationalStatus.add(2);
-						}
-					}
-				}
-			}
-		}
+		int childId = 0;
 
 		LinearLayout linearLayout = new LinearLayout(this);
 		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		try {
 
-		// proceeds
-		RelativeLayout relativeLayout = new RelativeLayout(this);
-		relativeLayout.setBackgroundColor(Color.GRAY);
-		relativeLayout.setId(1);
+			//
+			//
+			//
+			// оперативный
+			//
 
-		int childId = 0;
+			LinearLayout innerLinearLayout = new LinearLayout(this);
+			innerLinearLayout.setOrientation(LinearLayout.VERTICAL);
 
-		RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+			params.setMargins(10, 50, 10, 50);
+			innerLinearLayout.setLayoutParams(params);
 
-		RelativeLayout.LayoutParams textViewParameters = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+			// label
+			TextView textView = new TextView(this);
+			textView.setText("Оперативный");
+			textView.setId(++childId);
+			textView.setBackgroundColor(Color.parseColor("#43C6DB"));
+			textView.setGravity(Gravity.CENTER);
+			innerLinearLayout.addView(textView);
 
-		TextView textView = new TextView(this);
-		textView.setId(++childId);
-		textView.setText("test text");
-		// textView.setGravity(Gravity.CENTER_HORIZONTAL);
-		textView.setLayoutParams(textViewParameters);
-		relativeLayout.addView(textView);
+			TableLayout table = new TableLayout(this);
+			table.setStretchAllColumns(true);
+			table.setShrinkAllColumns(true);
 
-		ImageView imageView1 = new ImageView(this);
-		imageView1.setId(++childId);
-		imageView1.setImageResource(R.drawable.ic_launcher);
+			// images
+			TableRow currentRow = new TableRow(this);
+			TableRow.LayoutParams relativeLayoutParameters = new TableRow.LayoutParams(
+					TableRow.LayoutParams.WRAP_CONTENT,
+					TableRow.LayoutParams.WRAP_CONTENT, 1f);
 
-		RelativeLayout.LayoutParams imageViewParameters = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
-		imageViewParameters.addRule(RelativeLayout.BELOW, textView.getId());
-		relativeLayout.addView(imageView1, imageViewParameters);
+			DbHelper dbHelper = new DbHelper(this, null, null,
+					DbHelper.getDBVersion());
 
-		ImageView imageView2 = new ImageView(this);
-		imageView2.setImageResource(R.drawable.pailoid);
-		imageView2.setId(++childId);
-		RelativeLayout.LayoutParams imageViewParameters2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
-		imageViewParameters2.addRule(RelativeLayout.BELOW, textView.getId());
-		imageViewParameters2.addRule(RelativeLayout.RIGHT_OF,
-				imageView1.getId());
-		imageView2.setLayoutParams(imageViewParameters2);
-		relativeLayout.addView(imageView2);
+			// TODO make it as user options
+			String[] parameters = { "ОПЕРАТИВНЫЙ", "ABONTODAY" };
+			ArrayList<SmsRecord> recordsArray = dbHelper.getLastRecords(5,
+					parameters);
 
-		linearLayout.addView(relativeLayout, rlp);
+			ArrayList<Integer> daysNumber = new ArrayList<Integer>();
+			ArrayList<Integer> status = new ArrayList<Integer>();
 
-		// operational
-		relativeLayout = new RelativeLayout(this);
-		relativeLayout.setBackgroundColor(Color.BLUE);
+			currentRow = new TableRow(this);
+			TableRow days = new TableRow(this);
+			int added = 0;
 
-		relativeLayout.setId(2);
+			// get status for last 5 days
+			// TODO make days amount parameter
+			for (int i = 0; added < daysAmount && i < recordsArray.size(); i++) {
 
-		childId = 0;
+				if (recordsArray.get(i).getParameterName()
+						.equals("ОПЕРАТИВНЫЙ")) {
+					ImageView imageView = new ImageView(this);
+					imageView.setId(++childId);
+					imageView.setPadding(0, 0, 0, 0);
 
-		rlp = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
-		// rlp.addRule(RelativeLayout.BELOW, 1);
+					// TODO return to days of month
+					daysNumber.add(recordsArray.get(i).getDate()
+							.get(Calendar.DAY_OF_MONTH));
 
-		textViewParameters = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+					if (recordsArray.get(i).getParameterValue()
+							.matches("вчера")) {
+						status.add(0);
+					} else {
+						if (recordsArray.get(i).getParameterValue()
+								.equals("позавчера")) {
+							status.add(1);
+						} else {
+							status.add(2);
+						}
+					}
 
-		textView = new TextView(this);
-		textView.setId(++childId);
-		textView.setText("another test text");
-		// textView.setGravity(Gravity.CENTER_HORIZONTAL);
-		textView.setLayoutParams(textViewParameters);
-		relativeLayout.addView(textView);
+					added++;
 
-		imageView1 = new ImageView(this);
-		imageView1.setId(++childId);
-		imageView1.setImageResource(R.drawable.ic_launcher);
+				}
+			}
 
-		imageViewParameters = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
-		imageViewParameters.addRule(RelativeLayout.BELOW, textView.getId());
-		relativeLayout.addView(imageView1, imageViewParameters);
+			if (added == 0) {
+				TextView dataNotFound = new TextView(this);
+				dataNotFound.setGravity(Gravity.CENTER);
+				dataNotFound.setText("Данные не найдены");
+				dataNotFound.setBackgroundColor(Color.parseColor("#43C6DB"));
+				innerLinearLayout.addView(dataNotFound);
+			}
 
-		imageView2 = new ImageView(this);
-		imageView2.setImageResource(R.drawable.pailoid);
-		imageView2.setId(++childId);
-		imageViewParameters2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
-		imageViewParameters2.addRule(RelativeLayout.BELOW, textView.getId());
-		imageViewParameters2.addRule(RelativeLayout.RIGHT_OF,
-				imageView1.getId());
-		imageView2.setLayoutParams(imageViewParameters2);
-		relativeLayout.addView(imageView2);
+			if (status.size() == daysNumber.size()) {
+				for (int i = 0; i < status.size(); i++) {
 
-		linearLayout.addView(relativeLayout, rlp);
-		// ///////
+					TextView curDay = new TextView(this);
+					curDay.setGravity(Gravity.CENTER);
+					curDay.setText(String.valueOf(daysNumber.get(i)));
+					days.addView(curDay);
 
+					ImageView imageView = new ImageView(this);
+					switch (status.get(i)) {
+					case 0:
+						imageView.setImageResource(R.drawable.test);
+						break;
+					case 1:
+						imageView
+								.setImageResource(R.drawable.test_green_disk_red_and_white_rings);
+						break;
+					default:
+						imageView.setImageResource(R.drawable.red_disc);
+						break;
+					}
+					currentRow.addView(imageView);
+				}
+			}
+
+			// add images
+			if (added != 0) {
+				table.addView(days);
+				table.addView(currentRow);
+				table.setBackgroundColor(Color.parseColor("#43C6DB"));
+				innerLinearLayout.addView(table, relativeLayoutParameters);
+			}
+
+			linearLayout.addView(innerLinearLayout);
+
+			//
+			//
+			//
+			// выручка
+			//
+
+			innerLinearLayout = new LinearLayout(this);
+			innerLinearLayout.setOrientation(LinearLayout.VERTICAL);
+
+			params = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT,
+					LayoutParams.WRAP_CONTENT);
+			params.setMargins(10, 50, 10, 50);
+			innerLinearLayout.setLayoutParams(params);
+
+			// label
+			textView = new TextView(this);
+			textView.setText("ABONTODAY");
+			textView.setId(++childId);
+			textView.setBackgroundColor(Color.parseColor("#8800CCCC"));
+			textView.setGravity(Gravity.CENTER);
+			innerLinearLayout.addView(textView);
+
+			table = new TableLayout(this);
+			table.setStretchAllColumns(true);
+			table.setShrinkAllColumns(true);
+
+			// images
+			currentRow = new TableRow(this);
+			days = new TableRow(this);
+			added = 0;
+
+			relativeLayoutParameters = new TableRow.LayoutParams(
+					TableRow.LayoutParams.WRAP_CONTENT,
+					TableRow.LayoutParams.WRAP_CONTENT, 1f);
+
+			daysNumber.clear();
+			status.clear();
+
+			// TODO make days amount parameter
+			for (int i = 0; added < daysAmount && i < recordsArray.size(); i++) {
+
+				if (recordsArray.get(i).getParameterName().equals("ABONTODAY")) {
+					ImageView imageView = new ImageView(this);
+					imageView.setId(++childId);
+					imageView.setPadding(0, 0, 0, 0);
+
+					// TODO return to days of month
+					daysNumber.add(recordsArray.get(i).getDate()
+							.get(Calendar.DAY_OF_MONTH));
+
+					if (recordsArray.get(i).getParameterValue()
+							.matches("вчера")) {
+						status.add(0);
+					} else {
+						if (recordsArray.get(i).getParameterValue()
+								.equals("позавчера")) {
+							status.add(1);
+						} else {
+							status.add(2);
+						}
+					}
+
+					added++;
+
+				}
+			}
+
+			if (added == 0) {
+				TextView dataNotFound = new TextView(this);
+				dataNotFound.setGravity(Gravity.CENTER);
+				dataNotFound.setText("Данные не найдены");
+				dataNotFound.setBackgroundColor(Color.parseColor("#8800CCCC"));
+				innerLinearLayout.addView(dataNotFound);
+			}
+
+			if (status.size() == daysNumber.size()) {
+				for (int i = 0; i < status.size(); i++) {
+
+					TextView curDay = new TextView(this);
+					curDay.setGravity(Gravity.CENTER);
+					curDay.setText(String.valueOf(daysNumber.get(i)));
+					days.addView(curDay);
+
+					ImageView imageView = new ImageView(this);
+					switch (status.get(i)) {
+					case 0:
+						imageView.setImageResource(R.drawable.test);
+						break;
+					case 1:
+						imageView
+								.setImageResource(R.drawable.test_green_disk_red_and_white_rings);
+						break;
+					default:
+						imageView.setImageResource(R.drawable.red_disc);
+						break;
+					}
+					currentRow.addView(imageView);
+				}
+			}
+
+			// add images
+			if (added != 0) {
+				table.addView(days);
+				table.addView(currentRow);
+				table.setBackgroundColor(Color.parseColor("#8800CCCC"));
+				innerLinearLayout.addView(table, relativeLayoutParameters);
+			}
+
+			linearLayout.addView(innerLinearLayout);
+
+			//
+			// add all in layout
+		} catch (Exception ex) {
+			StackTraceElement[] s = ex.getStackTrace();
+			String str = "";
+			for (int i = 0; i < s.length; i++) {
+				str += s[i].toString() + " --- ";
+			}
+			TextView text = new TextView(this);
+			text.setText(ex.getMessage() + " --- " + str);
+			linearLayout.addView(text);
+		}
 		ScrollView scrollView = new ScrollView(this);
 		scrollView.setFillViewport(true);
 		scrollView.addView(linearLayout);
 
 		setContentView(scrollView);
+
+	}
+
+	public static int getDaysAmount() {
+		return daysAmount;
+	}
+
+	public static void setDaysAmount(int newDaysAmount) {
+		daysAmount = newDaysAmount;
 	}
 
 }
